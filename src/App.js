3c821d9c9f1654b2d603e6a6dfae22eb01ac3a38
Tanner Griffin <tannermgriffin@gmail.com>
2020-08-21 01:48:08 -0500

    Added form input change value test on user type

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
+import Search from './components/Search';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Search />
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/components/__tests__/Search.test.js
+++ src/components/__tests__/Search.test.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { render, fireEvent, waitFor, screen, queries } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event'
 import Search from '../Search';
 
 describe('Search', () => {
 	test('Search form rendered', async () => {
 		// Arrange
 		const { container, debug } = render(<Search />);
-		debug();
+		// debug();
 		// Act
 		// Assert
-		expect(container.querySelector('.form')).toHaveAttribute('id', 'search');
+		expect(container.querySelector('#search')).toBeTruthy();
+    });
+    
+    test('Input value changes when user types', async () => {
+		// Arrange
+		const { debug, getByPlaceholderText } = render(<Search />);
+		// debug();
+        // Act
+        await userEvent.type(getByPlaceholderText('First name'), 'Hello World!');
+		// Assert
+		expect(getByPlaceholderText('First name')).toHaveValue('Hello World!');
 	});
 });
