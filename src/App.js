b9438b8c9e6bd01bca1d57649ad4f3b55f67f14e
Tanner Griffin <tannermgriffin@gmail.com>
2020-08-25 12:40:19 -0500

    created sort header for table, sorting function for name email and phone

--- src/App.js
+++ src/App.js
@@ -9,40 +9,88 @@ import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 function App() {
-	const [search, setSearch] = useState('');
-	const [employees, setEmployees] = useState([]);
-	const [filter, setFilter] = useState([]);
+  const [search, setSearch] = useState('');
+  const [employees, setEmployees] = useState([]);
+  const [filter, setFilter] = useState([]);
+  const [order, setOrder] = useState({ ascending: false, field: '' });
 
-	useEffect(() => {
-		if (employees.length === 0) {
-			axios.get('https://randomuser.me/api/?results=200&nat=us').then((res) => {
-				setEmployees(res.data.results);
-				setFilter(res.data.results);
-			});
-		} else {
-			setFilter(
-				employees.filter(
-					(item) =>
+  useEffect(() => {
+    if (employees.length === 0) {
+      axios.get('https://randomuser.me/api/?results=200&nat=us').then((res) => {
+        setEmployees(res.data.results);
+        setFilter(res.data.results);
+      });
+    } else {
+      setFilter(
+        employees.filter(
+          (item) =>
             item.name.first.toLowerCase().includes(search) ||
             item.name.last.toLowerCase().includes(search) ||
-						item.email.includes(search) ||
-						item.phone.includes(search)
-				)
-			);
-		}
-	}, [search]);
+            item.email.includes(search) ||
+            item.phone.includes(search)
+        )
+      );
+    }
+  }, [search]);
 
-	return (
-		<div className='App'>
-			<Header />
-			<Search search={search} setsearch={setSearch} />
-				<Fade collapse top when={search}>
-					<h3 title='search-display'>Searching for {search}</h3>
-				</Fade>
-			<List employees={filter} />
+  function compare(a, b) {
+    let itemA, itemB;
+    if (order.field === 'name') {
+      itemA = a.name.first.toLowerCase();
+      itemB = b.name.first.toLowerCase();
+    } else if (order.field === 'email') {
+      itemA = a.email.toLowerCase();
+      itemB = b.email.toLowerCase();
+    } else if (order.field === 'phone') {
+      itemA = a.phone;
+      itemB = b.phone;
+    }
+
+    let comparison = 0;
+    if (order.ascending) {
+      if (itemA > itemB) {
+        comparison = 1;
+      } else if (itemA < itemB) {
+        comparison = -1;
+      }
+    } else {
+      if (itemA > itemB) {
+        comparison = -1;
+      } else if (itemA < itemB) {
+        comparison = 1;
+      }
+    }
+    return comparison;
+  }
+
+  const sort = (e) => {
+    if (e.target.id === 'sort-name') {
+      setOrder({ ...order, ascending: !order.ascending, field: 'name' });
+    } else if (e.target.id === 'sort-email') {
+      setOrder({ ...order, ascending: !order.ascending, field: 'email' });
+    } else if (e.target.id === 'sort-phone') {
+      setOrder({ ...order, ascending: !order.ascending, field: 'phone' });
+    }
+  };
+
+  useEffect(() => {
+    if (order.field !== '') {
+      const newFilter = filter.sort(compare);
+      setFilter([...newFilter]);
+    }
+  }, [order]);
+
+  return (
+    <div className='App'>
+      <Header />
+      <Search search={search} setsearch={setSearch} />
+      <Fade collapse top when={search}>
+        <h3 title='search-display'>Searching for {search}</h3>
+      </Fade>
+      <List employees={filter} sort={sort} />
       <Logo />
-		</div>
-	);
+    </div>
+  );
 }
 
 export default App;
--- src/components/List.js
+++ src/components/List.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import ListItem from './ListItem';
+import { Grid } from 'semantic-ui-react';
 
 export default function List(props) {
-	return (
-		<div id='employee-list'>
-			{props.employees.map((employee, i) => {
-				console.log(employee, i);
-				return (
-					<ListItem
-                        img={employee.picture.thumbnail}
-						name={employee.name.first + ' ' + employee.name.last}
-						email={employee.email}
-						number={employee.phone}
-						key={i}
-					/>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div id='employee-list'>
+      <Grid.Row id='sort'>
+        <Grid.Column>
+          <p>Sort</p>
+        </Grid.Column>
+        <Grid.Column>
+          <p id='sort-name' onClick={props.sort}>Name</p>
+        </Grid.Column>
+        <Grid.Column>
+          <p id='sort-email' onClick={props.sort}>Email</p>
+        </Grid.Column>
+        <Grid.Column id='number'>
+          <p id='sort-phone' onClick={props.sort}>Phone</p>
+        </Grid.Column>
+      </Grid.Row>
+      {props.employees.map((employee, i) => {
+        return (
+          <ListItem
+            img={employee.picture.thumbnail}
+            name={employee.name.first + ' ' + employee.name.last}
+            email={employee.email}
+            number={employee.phone}
+            key={i}
+          />
+        );
+      })}
+    </div>
+  );
 }
