232cf8a82e60a1d01d17f43ffc0b6dc51a9747a7
Tanner Griffin <tannermgriffin@gmail.com>
2020-08-24 19:21:23 -0500

    Added test for state change, displaying current name for searching below search form from state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import Search from './components/Search';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 
 function App() {
-  const [search, setSearch] = useState({firstname: '', lastname: ''});
+	const [search, setSearch] = useState({ firstname: '', lastname: '' });
 
 	return (
 		<div className='App'>
-			<Search search={search} setsearch={setSearch}/>
+			<Search search={search} setsearch={setSearch} />
+			{(search.firstname || search.lastname) && (
+				<h3 title='search-display'>
+					Searching for {search.firstname} {search.lastname}
+				</h3>
+			)}
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
 				<p>
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { render, fireEvent, waitFor, screen, queries } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test('renders learn react link', () => {
@@ -12,10 +12,21 @@ test('renders learn react link', () => {
 
 test('Input value changes when user types', async () => {
 	// Arrange
-  const { container, debug, getByPlaceholderText } = render(<App />);
+	const { container, debug, getByPlaceholderText } = render(<App />);
 	// debug();
 	// Act
-  await userEvent.type(getByPlaceholderText('First name'), 'Hello World!');
+	await userEvent.type(getByPlaceholderText('First name'), 'Hello World!');
 	// Assert
 	expect(getByPlaceholderText('First name')).toHaveValue('Hello World!');
+});
+
+test('Displayed search name changes on user typing', async () => {
+	// Arrange
+	const { container, debug, getByPlaceholderText, getByTitle } = render(<App />);
+	// debug();
+	// Act
+	await userEvent.type(getByPlaceholderText('First name'), 'Testy');
+	await userEvent.type(getByPlaceholderText('Last name'), 'Tester');
+	// Assert
+	expect(getByTitle('search-display')).toHaveTextContent('Searching for Testy Tester');
 });
