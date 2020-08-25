7fa4887c7a397652d5c9a257e759c164ad0560d4
Tanner Griffin <tannermgriffin@gmail.com>
2020-08-24 19:53:38 -0500

    changed jest-dom import to test circleci issue

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { render, fireEvent, waitFor, screen, queries } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import App from './App';
 
@@ -10,23 +10,28 @@ import App from './App';
 // 	expect(linkElement).toBeInTheDocument();
 // });
 
-test('Input value changes when user types', async () => {
-	// Arrange
+describe('Search form', () => {
+	test('Input value changes when user types', async () => {
+		// Arrange
+		
 	const { container, debug, getByPlaceholderText } = render(<App />);
-	// debug();
-	// Act
-	await userEvent.type(getByPlaceholderText('First name'), 'Hello World!');
-	// Assert
-	expect(getByPlaceholderText('First name')).toHaveValue('Hello World!');
-});
+		// debug();
+		// Act
+		await userEvent.type(getByPlaceholderText('First name'), 'Hello World!');
+		// Assert
+		expect(getByPlaceholderText('First name')).toHaveValue('Hello World!');
+	});
+	
+	test('Displayed search name changes on user typing', async () => {
+		// Arrange
+		const { container, debug, getByPlaceholderText, getByTitle } = render(<App />);
+		// debug();
+		// Act
+		await userEvent.type(container.querySelector('#firstname'), 'Testy');
+		await userEvent.type(container.querySelector('#lastname'), 'Tester');
+		// Assert
+		expect(getByTitle('search-display')).toHaveTextContent('Searching for Testy Tester');
+	});
+})
 
-test('Displayed search name changes on user typing', async () => {
-	// Arrange
-	const { container, debug, getByPlaceholderText, getByTitle } = render(<App />);
-	// debug();
-	// Act
-	await userEvent.type(getByPlaceholderText('First name'), 'Testy');
-	await userEvent.type(getByPlaceholderText('Last name'), 'Tester');
-	// Assert
-	expect(getByTitle('search-display')).toHaveTextContent('Searching for Testy Tester');
-});
+
--- src/components/Search.js
+++ src/components/Search.js
@@ -5,8 +5,8 @@ export default function Search(props) {
 	return (
 		<Form id="search">
 			<Form.Group widths='equal'>
-				<Form.Input fluid label='First name' placeholder='First name' onChange={(e) => props.setsearch({...props.search, firstname: e.target.value.trim()})}/>
-				<Form.Input fluid label='Last name' placeholder='Last name' onChange={(e) => props.setsearch({...props.search, lastname: e.target.value.trim()})}/>
+				<Form.Input fluid id='firstname' label='First name' placeholder='First name' onChange={(e) => props.setsearch({...props.search, firstname: e.target.value.trim()})}/>
+				<Form.Input fluid id='lastname' label='Last name' placeholder='Last name' onChange={(e) => props.setsearch({...props.search, lastname: e.target.value.trim()})}/>
 			</Form.Group>
 		</Form>
 	);
