f44317c555c2dc20c9489920067651d0083e49a8
Tanner Griffin <tannermgriffin@gmail.com>
2020-08-25 21:16:40 -0500

    Made sort header absolute, put list in sub scroll container. Refactored sort icon conditional and showing an icon if not selected

--- src/components/List.js
+++ src/components/List.js
@@ -1,78 +1,71 @@
 import React from 'react';
 import ListItem from './ListItem';
 import { Grid } from 'semantic-ui-react';
-import { TiArrowSortedDown, TiArrowSortedUp } from 'react-icons/ti';
+import { TiArrowSortedDown, TiArrowSortedUp, TiArrowUnsorted } from 'react-icons/ti';
 
 export default function List(props) {
-  return (
-    <div id='employee-list'>
-      <Grid.Row id='sort'>
-        <Grid.Column>
-          <p>Sort</p>
-        </Grid.Column>
-        <Grid.Column>
-          {props.order.field === 'name' ? (
-            props.order.ascending ? (
-              <p id='sort-name' onClick={props.sort}>
-                Name <TiArrowSortedUp />
-              </p>
-            ) : (
-              <p id='sort-name' onClick={props.sort}>
-                Name <TiArrowSortedDown />
-              </p>
-            )
-          ) : (
-            <p id='sort-name' onClick={props.sort}>
-              Name
-            </p>
-          )}
-        </Grid.Column>
-        <Grid.Column>
-          {props.order.field === 'email' ? (
-            props.order.ascending ? (
-              <p id='sort-email' onClick={props.sort}>
-                Email <TiArrowSortedUp />
-              </p>
-            ) : (
-              <p id='sort-email' onClick={props.sort}>
-                Email <TiArrowSortedDown />
-              </p>
-            )
-          ) : (
-            <p id='sort-email' onClick={props.sort}>
-              Email
-            </p>
-          )}
-        </Grid.Column>
-        <Grid.Column id='number'>
-          {props.order.field === 'phone' ? (
-            props.order.ascending ? (
-              <p id='sort-phone' onClick={props.sort}>
-                Phone <TiArrowSortedUp />
-              </p>
-            ) : (
-              <p id='sort-phone' onClick={props.sort}>
-                Phone <TiArrowSortedDown />
-              </p>
-            )
-          ) : (
-            <p id='sort-phone' onClick={props.sort}>
-              Phone
-            </p>
-          )}
-        </Grid.Column>
-      </Grid.Row>
-      {props.employees.map((employee, i) => {
-        return (
-          <ListItem
-            img={employee.picture.thumbnail}
-            name={employee.name.first + ' ' + employee.name.last}
-            email={employee.email}
-            number={employee.phone}
-            key={i}
-          />
-        );
-      })}
-    </div>
-  );
+	return (
+		<div id='employee-list'>
+			<Grid.Row id='sort'>
+				<Grid.Column>
+					<p>Sort</p>
+				</Grid.Column>
+				<Grid.Column>
+					<p id='sort-name' className='clickable' onClick={props.sort}>
+						Name{' '}
+						{props.order.field === 'name' ? (
+							props.order.ascending ? (
+								<TiArrowSortedUp />
+							) : (
+								<TiArrowSortedDown />
+							)
+						) : (
+							<TiArrowUnsorted />
+						)}
+					</p>
+				</Grid.Column>
+				<Grid.Column>
+					<p id='sort-email' className='clickable' onClick={props.sort}>
+						Email{' '}
+						{props.order.field === 'email' ? (
+							props.order.ascending ? (
+								<TiArrowSortedUp />
+							) : (
+								<TiArrowSortedDown />
+							)
+						) : (
+							<TiArrowUnsorted />
+						)}
+					</p>
+				</Grid.Column>
+				<Grid.Column id='number'>
+					<p id='sort-phone' className='clickable' onClick={props.sort}>
+						Phone{' '}
+						{props.order.field === 'phone' ? (
+							props.order.ascending ? (
+								<TiArrowSortedUp />
+							) : (
+								<TiArrowSortedDown />
+							)
+						) : (
+							<TiArrowUnsorted />
+						)}
+					</p>
+				</Grid.Column>
+			</Grid.Row>
+			<div id='employee-scroll'>
+				{props.employees.map((employee, i) => {
+					return (
+						<ListItem
+							img={employee.picture.thumbnail}
+							name={employee.name.first + ' ' + employee.name.last}
+							email={employee.email}
+							number={employee.phone}
+							key={i}
+						/>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
