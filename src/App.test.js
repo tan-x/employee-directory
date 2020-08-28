5cc53ba3e1db5b20b252c72d6dc36ec94c81c416
Tanner Griffin <tannermgriffin@gmail.com>
2020-08-28 18:19:42 -0500

    Fixed duplicate tests according to codeclimate warnings

--- src/App.test.js
+++ src/App.test.js
@@ -65,19 +65,15 @@ describe('Search form', () => {
 });
 
 describe('Rendering items', () => {
-  test('Header rendered', () => {
+  test('Header and Logo footer rendered', () => {
     const { container } = render(<App defFilter={dummyList} />);
     expect(container.querySelector('#header')).toBeTruthy();
-  });
-
-  test('Logo rendered', () => {
-    const { container } = render(<App defFilter={dummyList} />);
     expect(container.querySelector('#logo')).toBeTruthy();
   });
 });
 
 describe('Sorting list items', () => {
-  test('Items sorted by name', async () => {
+  test('Items sorted by name', () => {
     const { container, getByTestId } = render(<App defFilter={dummyList} />);
     // user clicks sort for ascending
     act(() => {
@@ -93,41 +89,7 @@ describe('Sorting list items', () => {
     expect(container.querySelector('#employee-scroll').lastChild).toHaveTextContent('Testy');
   });
 
-  test('Items sorted by phone', async () => {
-    const { container, getByTestId } = render(<App defFilter={dummyList} />);
-    // user clicks sort for ascending
-    act(() => {
-      fireEvent.click(getByTestId('sort-phone'));
-    });
-    expect(container.querySelector('#employee-scroll').firstChild).toHaveTextContent('444');
-    expect(container.querySelector('#employee-scroll').lastChild).toHaveTextContent('555');
-	// user clicks sort again for descending
-    act(() => {
-      fireEvent.click(getByTestId('sort-phone'));
-    });
-    expect(container.querySelector('#employee-scroll').firstChild).toHaveTextContent('555');
-    expect(container.querySelector('#employee-scroll').lastChild).toHaveTextContent('444');
-  });
-});
-
-describe('Sorting list items', () => {
-  test('Items sorted by name', async () => {
-    const { container, getByTestId } = render(<App defFilter={dummyList} />);
-    // user clicks sort for ascending
-    act(() => {
-      fireEvent.click(getByTestId('sort-name'));
-    });
-    expect(container.querySelector('#employee-scroll').firstChild).toHaveTextContent('Testy');
-    expect(container.querySelector('#employee-scroll').lastChild).toHaveTextContent('Zesty');
-	// user clicks sort again for descending
-    act(() => {
-      fireEvent.click(getByTestId('sort-name'));
-    });
-    expect(container.querySelector('#employee-scroll').firstChild).toHaveTextContent('Zesty');
-    expect(container.querySelector('#employee-scroll').lastChild).toHaveTextContent('Testy');
-  });
-
-  test('Items sorted by phone', async () => {
+  test('Items sorted by phone', () => {
     const { container, getByTestId } = render(<App defFilter={dummyList} />);
     // user clicks sort for ascending
     act(() => {
